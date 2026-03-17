57f2c7a335f4777f1dd05d45db4d7c20fa6600c9
sophia860 <sophia@pagegalleryjournal.com>
2026-03-17 13:22:54 +0000

    fix: improve scroll responsiveness - reduce Lenis duration and increase wheel multiplier

--- client/src/components/SmoothScroll.tsx
+++ client/src/components/SmoothScroll.tsx
@@ -4,12 +4,12 @@ import Lenis from 'lenis';
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      duration: 0.8, 
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: 'vertical',
       gestureOrientation: 'vertical',
       smoothWheel: true,
-      wheelMultiplier: 1,
+      wheelMultiplier: 11.5
       touchMultiplier: 2,
     });
 
