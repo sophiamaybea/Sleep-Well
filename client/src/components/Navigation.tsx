9120965e00ed97287b287487c84d56c81c16e976
sophia860 <sophia@pagegalleryjournal.com>
2026-03-18 23:30:21 +0000

    fix: point Navigation to transparent logo v2

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -85,7 +85,7 @@ export default function Navigation() {
       <nav className={`fixed top-0 left-0 w-full z-50 transition-all duration-500 ${scrolled ? 'py-4' : 'py-8'}`}>
         <div className="max-w-7xl mx-auto px-6 md:px-12 flex justify-between items-center">
           <Link href="/" className="relative group">
-<img src="/logo%20(1).png" alt="The Page Gallery Journal" className="h-10 w-auto" />
+<img src="/logo%20(2).png" alt="The Page Gallery Journal" className="h-10 w-auto" />
             <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-300 group-hover:w-full" />
           </Link>
 
