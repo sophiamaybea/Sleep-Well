88de86be0084b4322c7ab4bc68289daea6c95c41
sophia860 <sophia@pagegalleryjournal.com>
2026-03-28 01:41:39 +0000

    feat(seo): add ogTitle/ogDescription/ogType to WriterProfile usePageMeta (#10)

--- client/src/pages/WriterProfile.tsx
+++ client/src/pages/WriterProfile.tsx
@@ -52,7 +52,7 @@ export default function WriterProfile() {
     enabled: !!id,
     retry: false,
   });
-    usePageMeta({ title: data ? [data.user.firstName, data.user.lastName].filter(Boolean).join(' ') : 'Writer', description: data?.user ? `${[data.user.firstName, data.user.lastName].filter(Boolean).join(' ')}'s writing garden on The Page Gallery Journal.` : 'A writer on The Page Gallery Journal.' });
+    usePageMeta({ title: data ? [data.user.firstName, data.user.lastName].filter(Boolean).join(' ') : 'Writer', description: data?.user ? `${[data.user.firstName, data.user.lastName].filter(Boolean).join(' ')}'s writing garden on The Page Gallery Journal.` : 'A writer on The Page Gallery Journal.', ogTitle: data ? [data.user.firstName, data.user.lastName].filter(Boolean).join(' ') : 'Writer', ogDescription: data?.user.bio || (data ? [data.user.firstName, data.user.lastName].filter(Boolean).join(' ') + "'s writing on The Page Gallery Journal." : 'A writer on The Page Gallery Journal.'), ogType: 'profile', canonicalPath: '/writer/' + id });
 
   if (isLoading) {
     return (
